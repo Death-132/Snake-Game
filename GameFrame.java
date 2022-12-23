fc22ed518884c685c6f1ffe0658ea063ac3d6cc2
Death Seak <91606916+Death-132@users.noreply.github.com>
2022-12-23 11:13:06 +0530

    SuS

--- GameFrame.java
+++ GameFrame.java
@@ -6,7 +6,7 @@ public class GameFrame extends JFrame {
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.pa();
+        this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
     }
